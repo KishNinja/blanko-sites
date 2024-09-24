3a5dfe0dec5df0056bf00bfe67eb05269be92aa5
Vlad Kishkovar <kishkovarv@gmail.com>
2024-09-24 18:04:38 +0300

    Corection file and create fnc privar_strgin

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ def clear(s):
 
 def country(name):
     return f'{name} very good very nice'
+
 def add(one, two):
     print(one + two)
+
+def privat_string(string):
+    # Написано на cpp.
+    if len(string) >= 5:
+        return True
+    return False
