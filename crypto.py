801fd520cc40f575cc6ebd916946832983eff2c2
kishlst <bigbenkish3@gmail.com>
2024-09-25 16:25:47 +0300

    Create fnc btc

--- crypto.py
+++ crypto.py
@@ -6,3 +6,8 @@ response = requests.get(url)
 
 # data = response.json()
 print(response)
+
+def btc(res):
+    if res == url:
+        return 'Работа с btc пройдена удачно.'
+    return 'Работа с btc не началась'
