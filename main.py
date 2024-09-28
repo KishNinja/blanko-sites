ecb699a5b6f10555747b1503fdf59fc92316432f
kishlst <bigbenkish3@gmail.com>
2024-09-28 22:55:59 +0300

    Create fnc types

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ def palindrom(s):
     if s == s[::-1]:
         return True
     return False
+
+def types(j):
+    # Строки являются не изменяемым типом для того безопасности данных.
+    if type(j) == int:
+        return f'неизменяемые тип'
+    elif type(j) == str:
+        return f'неизменяемые тип'
+    elif type(j) == tuple:
+        return f'неизменяемые тип'
+    elif type(j) == float:
+        return f'неизменяемые тип'
+    elif type(j) == frozenset:
+        return f'неизменяемые тип'
+    elif type(j) == bytes:
+        return f'неизменяемые тип'
+    else:
+        return f'изменяемый тип'
+    
+s = 'hello'
