d8711976ce548ed026c90c4a19eb9092e971f534
kishlst <bigbenkish3@gmail.com>
2024-10-11 16:48:06 +0300

    Create write in file API and html

--- python_project/example.py
+++ python_project/example.py
@@ -3,7 +3,7 @@ from textwrap import indent
 import requests
 import main
 
-respone = requests.get('https://api.github.com')
+respone = requests.get('https://map.ukrainealarm.com/')
 
 # Метод ok выводит True если статус кода не превышает от 200 до 399.
 # Потому что 400 это ошибка пользователя, а 500 ошибка сервера.
@@ -12,9 +12,14 @@ def examination(s = respone.ok):
         return f'Correct code - {s}'
     return f'Wrong code - {s}'
 
-respone_json = respone.json()
-with open('/Users/vlad/blanko-sites/python_project/date.json', 'w', encoding='utf-8') as file:
-    # Записывать json в файл нужно только через метод dump.
-    main.json.dump(respone_json, file, indent=2, ensure_ascii=False)
-
-# В целом все начинают переходить на https запросы, ведь они намного безопасней чем http.
+try:
+    # В целом все начинают переходить на https запросы, ведь они намного безопасней чем http.
+    respone_json = respone.json()
+    with open('/Users/vlad/blanko-sites/python_project/date.json', 'w', encoding='utf-8') as file:
+        # Записывать json в файл нужно только через метод dump.
+        main.json.dump(respone_json, file, indent=2, ensure_ascii=False)
+except:
+    respone_text = respone.text
+    with open('/Users/vlad/blanko-sites/index.html', 'w', encoding='utf-8') as file:
+        # Записывать json в файл нужно только через метод dump.
+        file.write(respone_text)
