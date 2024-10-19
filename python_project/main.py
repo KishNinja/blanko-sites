5ce04e02396490d9400134bc330283546012338d
Vlad Kishkovar <kishkovarv@gmail.com>
2024-10-19 23:16:40 +0300

    Create file

--- python_project/main.py
+++ python_project/main.py
@@ -1,60 +1,24 @@
-import sys
-import os
-import math
-from random import randint
-import turtle
-import datetime
 import json
 import requests
+'''
+Типы (status_code)
+1xx-Иформационные
+2xx-Успешный
+3xx-Перенаправленные
+4xx-Клиентская ошибка
+5xx-Ошибка сервера
+'''
 
-time = datetime.datetime.now()
+qu = requests.get('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS57DltNK9zr13gCil9CR26p7ks3iVKut84MA&s')
 
-___ = datetime.datetime.now()
+try:
+    with open('headers-photo.json', 'w', encoding='utf-8') as file:
+        json.dump(dict(qu.headers), file, indent=2)
+except:
+    pass
 
-with open('python_project/date.json', 'r', encoding='utf-8') as file:
-    oxxxymiron_json = json.load(file)
-
-class Phone:
-    def __init__(self, name, color, price, orm, memory) -> None:
-        self.name = name
-        self.color = color
-        self.price = price
-        self.orm = orm
-        self.memory = memory
-    # В метод __str__ не записуется аргументы кроме self.
-    def __str__(self):
-        return f'Name phone - {self.name}\nColor phone - {self.color}\nPrice phone - {self.price}\nCount orm - {self.price}\nCount memory - {self.price}'
-    
-    def __norma_phone__(self):
-        total = self.memory / self.orm
-        if total > 10:
-            return 'Norma'
-        return 'No norma, It phone old'
-
-class Iphone(Phone):
-    def __init__(self, name, color, price, orm, memory, ios):
-        super().__init__(name, color, price, orm, memory)
-        self.ios = ios
-    def __str__(self):
-        # super это обращение к предыдущему куску кода который находится в 
-        # классе родитель.
-        return f'{super().__str__()}\nIOS - {self.ios}'
-
-class Enemy():
-    def __init__(self, name, hp) -> None:
-        self.name = name
-        self.hp = hp 
-
-class Guns():
-    def __init__(self, damage) -> None:
-        self.damege = damage
-
-    # Когда мы передаем в метод объект, то нам предаставляются вся информация объекта.
-    def __damage_fnc__(self, emeny) -> None:
-        i = 0
-        while emeny.hp > 0:
-            emeny.hp -= self.damege  
-            i += 1
-            if emeny.hp == 0:
-                break
-        return f'Было нанесено {i} удара, {emeny.name} убит.'
+try:
+    with open('photo.jpeg', 'wb') as file:
+        file.write(qu.content)
+except:
+    pass
